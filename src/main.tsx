a83382ecd86f69ec3c61908649d9f9cc9652572e
[dyad] <git@dyad.sh>
2025-09-07 04:34:44 -0700

    [dyad] Wrapped app with SessionContextProvider - wrote 1 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { supabase } from './lib/supabaseClient';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
+  </React.StrictMode>
+);
