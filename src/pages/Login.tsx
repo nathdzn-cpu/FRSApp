dce8ffac9baa426b1861669b7416c7d3f62120ef
[dyad] <git@dyad.sh>
2025-09-07 06:58:20 -0700

    [dyad] Changed login page background to red - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -44,7 +44,7 @@ const LoginPage: React.FC = () => {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 p-4">
+    <div className="min-h-screen flex items-center justify-center bg-red-500 p-4">
       <Card className="w-full max-w-md">
         <CardHeader className="text-center">
           <CardTitle className="text-3xl font-bold">FRS Haulage Login</CardTitle>
