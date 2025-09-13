7741736599a9923a856222df870da6db2b6f5e4c
[dyad] <nathdzn@gmail.com>
2025-09-13 20:09:13 +0100

    Reverted all changes back to version bfae29b6002edd310369e2f484ca7da5a0101b85

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,13 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.haulageapp',
-  appName: 'HOSS',
-  webDir: 'dist',
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 0,
-    },
-  },
+  appName: 'Haulage App',
+  webDir: 'dist'
 };
 
 export default config;
